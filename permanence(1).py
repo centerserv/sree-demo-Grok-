0a98bd2f897d1b862a6be4862bf1dd76a8620f4f
centerserv <amorin@centerserv.com>
2025-08-06 09:59:21 -0400

    Delete permanence(1).py

--- permanence(1).py
+++
@@ -1,5 +0,0 @@
-def update_trust(prior_trust, accuracy):
-    """Update trust score using Bayesian-inspired method."""
-    likelihood = accuracy
-    trust = (prior_trust * likelihood) / (prior_trust * likelihood + (1 - prior_trust) * (1 - likelihood))
-    return trust
