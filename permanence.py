50305ee43a31b7a50a09d604af2263f9ef995eb0
centerserv <amorin@centerserv.com>
2025-08-08 13:12:29 -0400

    Update permanence.py

--- permanence.py
+++ permanence.py
@@ -1,6 +1,6 @@
-def update_trust(prior_trust, accuracy, decay=0.75):
+def update_trust(prior_trust, accuracy, decay=0.9):
     likelihood = accuracy
     if accuracy < prior_trust:
         likelihood *= decay
     trust = (prior_trust * likelihood) / (prior_trust * likelihood + (1 - prior_trust) * (1 - likelihood))
-    return max(0.5, min(trust, 0.95))
+    return max(0.5, min(trust, 0.96))
